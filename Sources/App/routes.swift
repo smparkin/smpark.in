273510547cf195e9d5a5da16193972bf113c1e2c
Stephen <smparkin@calpoly.edu>
2019-11-04 21:03:48 -0800

    just something to test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,15 +2,10 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // "It works" page
-    router.get { req in
-        return try req.view().render("welcome")
+    router.get("/") { request in
+        return try request.view().render("home")
     }
-    
-    // Says hello
-    router.get("hello", String.parameter) { req -> Future<View> in
-        return try req.view().render("hello", [
-            "name": req.parameters.next(String.self)
-        ])
+    router.get("/index.html") { request in
+        return try request.view().render("home")
     }
 }
