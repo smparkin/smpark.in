926f230007f31d8a10005b27fff60701aa37a0bd
Stephen <smparkin@calpoly.edu>
2019-11-05 15:53:32 -0800

    404

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,16 @@ import Vapor
 
 // Register your application's routes here.
 public func routes(_ router: Router) throws {
-    router.get("/") { request in
+    router.get("") { request in
         return try request.view().render("home")
     }
-    router.get("/index.html") { request in
+    router.get("index.html") { request in
         return try request.view().render("home")
     }
-    router.get("/privacy") { request in
+    router.get("privacy") { request in
         return try request.view().render("privacy")
+    }
+    router.get("404") { request in
+        return try request.view().render("404")
     }
 }
