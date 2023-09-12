7f3586cd36533400b8948569f45100120394a64a
Stephen Parkinson <smp827@gmail.com>
2023-09-11 21:08:38 -0700

    vapor again

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "smparkin",
+    platforms: [
+       .macOS(.v12)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Leaf", package: "leaf")
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .executableTarget(
+            name: "Run",
+            dependencies: [
+                .target(name: "App")
+            ]
+        ),
+        .testTarget(
+            name: "AppTests",
+            dependencies: [
+                .target(name: "App"),
+                .product(name: "XCTVapor", package: "vapor")
+            ]
+        )
+    ]
+)
+
